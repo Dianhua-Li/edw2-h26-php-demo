7423ad1a40c00ce6618a3454d7a4383d116d643e
Doran(Dianhua) Li <lidianhua0@gmail.com>
2026-06-19 13:22:47 -0400

    open logs

--- public/index.php
+++ public/index.php
@@ -1,5 +1,8 @@
 <?php
 
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
 require_once __DIR__ . '/../app/db.php';
 
 $statement = $pdo->query("SELECT * FROM messages ORDER BY created_at DESC");
